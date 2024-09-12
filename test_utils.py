8c341c857dcd82f7569d926820baee802f4cb4a7
jason040510 <144149601+jason040510@users.noreply.github.com>
2024-09-12 17:51:47 -0400

    Add files via upload

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,3 @@
-## Please fill in all the parts labeled as ### YOUR CODE HERE
-
 import numpy as np
 import pytest
 from utils import *
@@ -14,18 +12,22 @@ def test_dot_product():
     
 def test_cosine_similarity():
     ### YOUR CODE HERE
+    vector1 = np.array([1, 2, 3])
+    vector2 = np.array([1, 2, 3])
     
-    result = ### YOUR CODE HERE
+    result = cosine_similarity(vector1, vector2)
     
-    expected_result = ### YOUR CODE HERE
+    expected_result = 1
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
     ### YOUR CODE HERE
+    vector1 = np.array([1, 2, 3])
+    vector2 = np.array([1, 2, 3])
     
-    result = ### YOUR CODE HERE
+    result = nearest_neighbor(vector1, vector2)
     
-    expected_index = ### YOUR CODE HERE
+    expected_index = 0
     
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
